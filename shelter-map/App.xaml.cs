693b182c2232b77de8dbe2ecdce45bf618b60906
Janet Spellman <janetspellman13@gmail.com>
2026-03-25 23:15:05 -0700

    Add project files.

---
+++ shelter-map/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Esri.ArcGISRuntime;
+using System.Configuration;
+using System.Data;
+using System.Text.Json;
+using System.Windows;
+using System.IO;
+
+
+
+namespace shelter_map
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var json = File.ReadAllText("appsettings.json");
+            var config = JsonDocument.Parse(json);
+            var apiKey = config.RootElement.GetProperty("ArcGisApiKey").GetString();
+
+
+            ArcGISRuntimeEnvironment.ApiKey = apiKey;
+        }
+    }
+
+}
